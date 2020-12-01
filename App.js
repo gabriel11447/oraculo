c6a64751dd032f3080f958a7f1526a94004fb77f
Gabriel de Carvalho <gabriel.de.carvalho_@hotmail.com>
2020-12-01 09:15:49 -0300

    dev: Create react-native project

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+        <View>
+          <Text>
+            Oráculo
+          </Text>
+        </View>
+    </>
+  );
+};
