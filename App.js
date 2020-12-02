4ea188e18309a1fb9423ad41a08cd6b436e1b6ff
Felipe Bis <felipe.bis@aluno.ifsp.edu.br>
2020-12-02 18:04:27 -0300

    dev: making progress

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from "./src/pages/Home";
+import Main from "./src/pages/Main";
 
-import Home from "./pages/Home";
-import Main from "./pages/Main";
+function HomeScreen() {
+  return (
+    <Home/>
+  );
+}
+
+function MainScreen(){
+  return(<Main/>);
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <Main />
+  <NavigationContainer>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Main" component={MainScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 };
